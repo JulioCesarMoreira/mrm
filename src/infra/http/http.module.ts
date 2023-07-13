1655374cdb699228c30984120bfda19d142aa9d4
julio <juliopaesmoreira@hotmail.com>
2023-07-13 13:26:38 -0300

    fix(http): get out unecessary database import in controller module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,10 +1,8 @@
-import { DatabaseModule } from '@infra/service/database/database.module';
 import { Module } from '@nestjs/common';
 import { TenantController } from './controllers';
 import { CreateTenantUseCase } from '@application/use-cases/tenant';
 
 @Module({
-  imports: [DatabaseModule],
   controllers: [TenantController],
   providers: [CreateTenantUseCase],
 })
