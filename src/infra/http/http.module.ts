eb17f4d6d5a5b53ed2e6551472afd731d3dca461
julio <juliopaesmoreira@hotmail.com>
2023-07-15 14:07:10 -0300

    fix(client model, tenant fix): modifed model to a correct colunms and fix the tenant

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TenantController } from './controllers';
 import { CreateTenantUseCase } from '@application/use-cases/tenant';
+import { DatabaseModule } from '@infra/service/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [TenantController],
   providers: [CreateTenantUseCase],
 })
