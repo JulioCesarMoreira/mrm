354379a80cfb6f3ae785290fb3181c03df73e2a1
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2023-07-31 16:01:25 -0300

    feat(Frontend, Components): added visibility and hidro ponta svgs

--- frontend/src/components/Svg/SvgLibrary.tsx
+++ frontend/src/components/Svg/SvgLibrary.tsx
@@ -1,1 +1,3 @@
-export { default as hidro_ponta } from "../../assets/svgs/hidro_ponta.svg";
+export { default as hidro_ponta } from '../../assets/svgs/hidro_ponta.svg';
+export { default as visibility } from '../../assets/svgs/visibility.svg';
+export { default as visibility_off } from '../../assets/svgs/visibility_off.svg';
