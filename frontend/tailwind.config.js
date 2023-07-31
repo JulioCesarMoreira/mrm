85ab00fe5d7273bf19bdbb4a29bd6fceb40d9f3a
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2023-07-31 16:00:36 -0300

    chore: setting files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-			colors: {
-			'main-blue': '#0100CA',
-			}
-		},
-},
+      colors: {
+        'main-blue': '#0100CA',
+        'hidro-blue-300': '#006aff',
+        'dark-blue': '#1C232C',
+
+        'gray-d9': '#D9D9D9',
+        'gray-scale-300': '52575F',
+        'gray-scale-500': '797E86',
+      },
+    },
+  },
   plugins: [],
 };
