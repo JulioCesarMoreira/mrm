1ba0e15ee0094d9de3ae80af663db72dfaf5bb30
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2023-07-31 16:10:58 -0300

    feat(Frontend, Login): started login page

---
+++ frontend/src/components/LoginCard/LoginCard.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from 'react';
+import { ChildrenProperty } from '../../types';
+
+export default function LoginCard({
+  children,
+}: ChildrenProperty): ReactElement {
+  return (
+    <div className="bg-gray-d9 flex-col-center rounded-lg p-6">
+      <p className="text-dark-blue text-2xl font-bold">Login</p>
+
+      <div className="flex-center h-full w-full">{children}</div>
+    </div>
+  );
+}
---
+++ frontend/src/types.ts
@@ -0,0 +1,5 @@
+import { ReactNode } from 'react';
+
+export interface ChildrenProperty {
+  children: ReactNode;
+}
