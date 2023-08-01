e2dcec602c451ec3f9d9743ceb0e339f4ed1c5c3
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2023-08-01 14:59:50 -0300

    feat(Frontend, Tailwind): added colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,8 +8,10 @@ module.exports = {
         'dark-blue': '#1C232C',
 
         'gray-d9': '#D9D9D9',
-        'gray-scale-300': '52575F',
-        'gray-scale-500': '797E86',
+        'gray-scale-300': '#52575F',
+        'gray-scale-500': '#797E86',
+
+        'red-auxiliary': '#CB0000',
       },
     },
   },
