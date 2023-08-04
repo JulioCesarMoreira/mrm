23c4dda53c760a37a215c383b129001b363cc1e7
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2023-08-04 20:18:31 -0300

    feat(Frontend, Svgs): added sideMenu svgs

--- frontend/src/components/Svg/Svg.tsx
+++ frontend/src/components/Svg/Svg.tsx
@@ -1,13 +1,15 @@
 import { createElement, ReactElement } from 'react';
 import * as Svgs from './SvgLibrary';
 
+export type SvgName = keyof typeof Svgs;
+
 export interface SvgProperties extends React.SVGProps<SVGSVGElement> {
-	name: keyof typeof Svgs
+  name: SvgName;
 }
 
 export default function Svg({
-	name,
-	...properties
+  name,
+  ...properties
 }: SvgProperties): ReactElement {
-	return createElement(Svgs[name], properties);
+  return createElement(Svgs[name], properties);
 }
