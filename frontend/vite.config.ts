dc3fee27c82e7a5a13371093d08ed978b52fe721
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2023-08-14 04:34:06 -0300

    chore(Frontend, config): added aliases to paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [
     react(),
     svgr({
-      exportAsDefault: true
-    })
-  ]
-})
+      exportAsDefault: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@lib': '/src/lib',
+    },
+  },
+});
