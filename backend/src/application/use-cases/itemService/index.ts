7a3acbfc82a93ce52d0ed2ae7f8b09a516e2da23
julio <juliopaesmoreira@hotmail.com>
2023-08-23 21:48:26 -0300

    feat(itemService): creating use cases

---
+++ backend/src/application/use-cases/itemService/index.ts
@@ -0,0 +1,5 @@
+export * from './create-itemService.use-case';
+export * from './get-itemService.use-case';
+export * from './fetch-itemService.use-case';
+export * from './update-itemService.use-case';
+export * from './delete-itemService.use-case';
---
+++ backend/src/application/use-cases/itemService/update-itemService.use-case.ts
@@ -0,0 +1,19 @@
+import { ItemService } from '@application/core/entities';
+import { ItemServiceRepository } from '@application/core/repositories';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UpdateItemServiceUseCase {
+  constructor(private itemServiceRepository: ItemServiceRepository) {}
+
+  async updateItemService(
+    itemServiceId: number,
+    itemServiceFields: Omit<ItemService, 'id' | 'tenantId'>,
+  ): Promise<ItemService> {
+    const updatedItemService = await this.itemServiceRepository.update(
+      itemServiceId,
+      itemServiceFields,
+    );
+    return updatedItemService;
+  }
+}
