c74bd991f21d368db893cb56a7b7254a7c7b1463
julio <juliopaesmoreira@hotmail.com>
2023-08-26 20:13:27 -0300

    fix(use case): transform the string in date into use case

--- backend/src/application/use-cases/proposalService/create-proposalService.use-case.ts
+++ backend/src/application/use-cases/proposalService/create-proposalService.use-case.ts
@@ -9,6 +9,12 @@ export class CreateProposalServiceUseCase {
   async createProposalService(
     proposalService: Omit<ProposalService, 'id'>,
   ): Promise<ProposalService> {
+    const { periodValidity, sendDate } = proposalService;
+
+    // converting the received dates in to a DATE type
+    proposalService.periodValidity = new Date(periodValidity);
+    proposalService.sendDate = new Date(sendDate);
+
     const createdProposalService = await this.proposalServiceRepository.create(
       proposalService,
     );
