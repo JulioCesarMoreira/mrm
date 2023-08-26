525bbb44f9d62ff46c8e6925a752b0d7990472f1
julio <juliopaesmoreira@hotmail.com>
2023-08-26 19:10:54 -0300

    feat(dtos): proposal service dtos

---
+++ backend/src/infra/http/dtos/proposalService/proposalService.dto.ts
@@ -0,0 +1,117 @@
+import {
+  IsNotEmpty,
+  IsUUID,
+  IsNumberString,
+  IsOptional,
+  IsDateString,
+  IsInt,
+  IsBoolean,
+} from 'class-validator';
+
+export class CreateProposalServiceDto {
+  @IsOptional()
+  @IsDateString()
+  sendDate: Date;
+
+  @IsOptional()
+  @IsInt()
+  installmentsBalance: number;
+
+  @IsDateString()
+  periodValidity: Date;
+
+  @IsInt()
+  @IsOptional()
+  discount: number;
+
+  @IsInt()
+  @IsOptional()
+  percentageEntry: number;
+
+  @IsInt()
+  @IsOptional()
+  guaranteePeriod: number;
+
+  @IsBoolean()
+  @IsOptional()
+  approved: boolean;
+
+  @IsInt()
+  @IsNotEmpty()
+  clientId: number;
+
+  @IsUUID()
+  @IsNotEmpty()
+  tenantId: string;
+}
+
+export class GetProposalServiceIdDto {
+  @IsNumberString()
+  id: number;
+}
+
+export class FetchProposalServicesDto {
+  @IsOptional()
+  @IsDateString()
+  sendDate: Date;
+
+  @IsOptional()
+  @IsInt()
+  installmentsBalance: number;
+
+  @IsDateString()
+  periodValidity: Date;
+
+  @IsInt()
+  @IsOptional()
+  discount: number;
+
+  @IsInt()
+  @IsOptional()
+  percentageEntry: number;
+
+  @IsInt()
+  @IsOptional()
+  guaranteePeriod: number;
+
+  @IsBoolean()
+  @IsOptional()
+  approved: boolean;
+
+  @IsInt()
+  @IsOptional()
+  clientId: number;
+
+  @IsUUID()
+  @IsOptional()
+  tenantId: string;
+}
+
+export class UpdateProposalServiceDto {
+  @IsOptional()
+  @IsDateString()
+  sendDate: Date;
+
+  @IsOptional()
+  @IsInt()
+  installmentsBalance: number;
+
+  @IsDateString()
+  periodValidity: Date;
+
+  @IsInt()
+  @IsOptional()
+  discount: number;
+
+  @IsInt()
+  @IsOptional()
+  percentageEntry: number;
+
+  @IsInt()
+  @IsOptional()
+  guaranteePeriod: number;
+
+  @IsBoolean()
+  @IsOptional()
+  approved: boolean;
+}
