7605b7978210027e1c3cb5d76591f606c7dee10f
julio <juliopaesmoreira@hotmail.com>
2023-08-28 14:15:02 -0300

    feat(use cases): well use cases

---
+++ backend/src/application/use-cases/well/update-well.use-case.ts
@@ -0,0 +1,23 @@
+import { Well } from '@application/core/entities';
+import { WellRepository } from '@application/core/repositories';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UpdateWellUseCase {
+  constructor(private wellRepository: WellRepository) {}
+
+  async updateWell(
+    wellId: number,
+    wellFields: Omit<Well, 'id' | 'proposalServiceId'>,
+  ): Promise<Well> {
+    const { deliveryDate } = wellFields;
+
+    // converting date to save in RDS
+    wellFields.deliveryDate = deliveryDate
+      ? new Date(deliveryDate)
+      : deliveryDate;
+
+    const updatedWell = await this.wellRepository.update(wellId, wellFields);
+    return updatedWell;
+  }
+}
