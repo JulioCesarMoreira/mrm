3a56aa7d10a47a5da51091829186daeabe2ee86a
julio <juliopaesmoreira@hotmail.com>
2023-09-10 19:39:43 -0300

    test(client): test do projeto file

--- backend/__tests__/client.api.spec.ts
+++ backend/__tests__/client.api.spec.ts
@@ -1,26 +1,78 @@
 import { ClientRepository } from '@application/core/repositories';
 import { InMemoryClientRepository } from '@infra/service/database/inMemory/repositories/inMemory-client.repository';
-import * as request from 'supertest';
 import { bootstrap } from '__tests__/utils/bootstrap';
 import { INestApplication } from '@nestjs/common';
+import generateFakeCpf from './utils/cpfFaker';
+import * as request from 'supertest';
 
+// Container de testes
 describe('Client routes e2e testing', () => {
   let app: INestApplication;
   const path = '/client';
   let clientId: number;
 
+  // Inicialização da API
   beforeAll(async () => {
     app = await bootstrap(ClientRepository, InMemoryClientRepository);
     app.init();
   });
 
+  // Objeto válido que será aceito pela API
   const payload = {
-    contactName: 'Cliente test',
-    contactPhone: '42999999999',
-    cpfCnpj: '58677087000116',
     name: 'Nome test',
+    cpfCnpj: generateFakeCpf(),
+    contactName: 'Cliente teste 1',
+    contactPhone: '42999999999',
     tenantId: '3d222283-d485-4b54-acb8-5f290c105143',
   };
+
+  // Caso de teste a ser executado
+  it('should not CREATE client with a invalid CNPJ or CPF', async () => {
+    const cnpjFailPlayload = {
+      name: 'Nome test',
+      cpfCnpj: '00000000000000', // 000.000.000/0000-00
+      contactName: 'Cliente teste 1',
+      contactPhone: '42999999999', // 00 0 0000-0000
+      tenantId: '3d222283-d485-4b54-acb8-5f290c105143', // uuid
+    };
+
+    const cnpjResponse = await request(app.getHttpServer())
+      .post(path)
+      .send(cnpjFailPlayload);
+
+    const expectedError = [{ field: 'cpfCnpj', error: 'Invalid cpf or cnpj' }];
+
+    const receivedError = cnpjResponse.body.message;
+
+    expect(cnpjResponse.statusCode).toBe(400);
+    expect(receivedError).toEqual(expectedError);
+  });
+
+  it.only('should not CREATE client with out the name of company', async () => {
+    const cnpjFailPlayload = {
+      cpfCnpj: generateFakeCpf(), // 000.000.000/0000-00
+      contactName: 'Cliente teste 1',
+      contactPhone: '42999999999', // 00 0 0000-0000
+      tenantId: '3d222283-d485-4b54-acb8-5f290c105143', // uuid
+    };
+
+    const cnpjResponse = await request(app.getHttpServer())
+      .post(path)
+      .send(cnpjFailPlayload);
+
+    const expectedError = [
+      {
+        field: 'name',
+        error:
+          'name must be shorter than or equal to 191 characters, name must be a string',
+      },
+    ];
+
+    const receivedError = cnpjResponse.body.message;
+
+    expect(cnpjResponse.statusCode).toBe(400);
+    expect(receivedError).toEqual(expectedError);
+  });
 
   it('should CREATE client with a valid CNPJ', async () => {
     const response = await request(app.getHttpServer())
--- backend/__tests__/utils/bootstrap.ts
+++ backend/__tests__/utils/bootstrap.ts
@@ -1,6 +1,11 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
 import { Test } from '@nestjs/testing';
+import { ValidationError } from 'class-validator';
 import { AppModule } from 'src/app.module';
 import { GlobalExceptionFilter } from 'src/infra/global-exception/global-exception.filter';
 
@@ -16,7 +21,24 @@ export async function bootstrap(
     .compile();
 
   const app = moduleRef.createNestApplication();
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: false },
+      forbidNonWhitelisted: true,
+      whitelist: true,
+      exceptionFactory: (
+        validationErrors: ValidationError[] = [],
+      ): BadRequestException => {
+        return new BadRequestException(
+          validationErrors.map((error) => ({
+            field: error.property,
+            error: Object.values(error.constraints).join(', '),
+          })),
+        );
+      },
+    }),
+  );
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new GlobalExceptionFilter(httpAdapter));
