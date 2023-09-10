f88c3fa909a88d43c35e1778302216515f6d93fc
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2023-09-10 17:10:50 -0300

    feat(Frontend, hooks): added more errors handling

--- frontend/src/hooks/useOnError.ts
+++ frontend/src/hooks/useOnError.ts
@@ -13,11 +13,39 @@ interface OnError {
   handleError: (error: unknown) => void;
 }
 
+interface DBError {
+  field: {
+    target: string;
+  };
+  mysqlError?: string;
+}
+
 export default function useOnError(): OnError {
   const { toast } = useToast();
 
   function handleError(error: unknown): void {
+    console.log('Error:', error);
+
     const axiosError = error as AxiosError;
+
+    if (axiosError.message === 'Network Error') {
+      toast({
+        title: 'Network Error',
+        variant: 'destructive',
+      });
+      return;
+    }
+
+    const dbError = axiosError.response?.data as DBError;
+
+    if (dbError.mysqlError) {
+      toast({
+        title: dbError.field.target,
+        variant: 'destructive',
+      });
+      return;
+    }
+
     const customError = axiosError.response?.data as CustomError | ErrorMessage;
 
     const title = (customError as CustomError).message[0]?.error
@@ -28,7 +56,6 @@ export default function useOnError(): OnError {
       title,
       variant: 'destructive',
     });
-    console.log('Error:', error);
   }
 
   return { handleError };
