da3f61c6c45b2a75c9a13571de3c9490e5d5ece8
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2023-09-11 00:29:39 -0300

    Feat(Frontend/Clients): added missing filter inputs

--- frontend/src/pages/Clients/components/Filters.tsx
+++ frontend/src/pages/Clients/components/Filters.tsx
@@ -16,14 +16,31 @@ export default function Filters(): ReactElement {
 
   return (
     <div className="border-gray-scale-800 flex !h-[100px] !min-h-[100px] w-full border-b p-4 pt-2">
-      <FormWrapper<ClientsFilter> id="filters-form" onSubmit={onSubmitFilters}>
+      <FormWrapper<ClientsFilter>
+        id="filters-form"
+        onSubmit={onSubmitFilters}
+        className="flex gap-4"
+      >
         <Input.Wrapper className="w-[300px]">
           <Input.Label label="Nome" />
-          <Input.Field id="name" name="name" placeholder="Nome">
-            <Search className="stroke-gray-scale-500 mr-2 h-4 w-4" />
-          </Input.Field>
+          <Input.Field id="name" name="name" placeholder="Nome" />
+        </Input.Wrapper>
+
+        <Input.Wrapper className="w-[300px]">
+          <Input.Label label="Contato" />
+          <Input.Field
+            id="contactName"
+            name="contactName"
+            placeholder="Busque por contato"
+          />
+        </Input.Wrapper>
+
+        <Input.Wrapper className="w-[300px]">
+          <Input.Label label="Telefone" />
+          <Input.Field id="contactPhone" name="contactPhone" maskType="tel" />
         </Input.Wrapper>
       </FormWrapper>
+
       <div className="flex w-full flex-row-reverse items-center pt-3">
         <Tooltip position="left" text="Pesquisar">
           <Button
