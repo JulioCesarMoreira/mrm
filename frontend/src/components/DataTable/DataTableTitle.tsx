a46cf9ad38d43bd10e9681be64d8c010673d3d1b
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2023-09-15 18:38:11 -0300

    feat(Frontend/Components): added disabled add state

--- frontend/src/components/DataTable/DataTableTitle.tsx
+++ frontend/src/components/DataTable/DataTableTitle.tsx
@@ -3,22 +3,45 @@ import { Button } from '@components/ui/button';
 import type { ReactElement } from 'react';
 import type { DataTableTitleProperties } from './types';
 import { DialogTrigger } from '@components/ui/dialog';
+import { twMerge } from 'tailwind-merge';
+import Tooltip from '@components/Tooltip/Tooltip';
 
 export default function DataTableTitle({
   title,
   addElementButtonLabel,
+  disabledAdd,
+  disabledMessage,
 }: DataTableTitleProperties): ReactElement {
   return (
     <div className="flex items-center justify-between py-4">
-      <p className="text-gray-scale-400 text-xl font-semibold">{title}</p>
-
-      {addElementButtonLabel && (
-        <DialogTrigger>
-          <Button className="bg-hidro-blue-300 hover:bg-main-blue flex gap-4 rounded-md text-white">
-            <Plus size={18} /> {addElementButtonLabel}
-          </Button>
-        </DialogTrigger>
+      {title && (
+        <p className="text-gray-scale-400 text-xl font-semibold">{title}</p>
       )}
+
+      {addElementButtonLabel &&
+        (disabledAdd ? (
+          <Tooltip text={disabledMessage as string} position="bottom">
+            <Button
+              className={twMerge(
+                '!bg-gray-scale-800 text-gray-scale-500 flex !cursor-not-allowed select-none gap-4 rounded-md',
+                !title ? 'ml-auto' : '',
+              )}
+            >
+              <Plus size={18} /> {addElementButtonLabel}
+            </Button>
+          </Tooltip>
+        ) : (
+          <DialogTrigger asChild>
+            <Button
+              className={twMerge(
+                'bg-hidro-blue-300 hover:bg-main-blue flex gap-4 rounded-md text-white',
+                !title ? 'ml-auto' : '',
+              )}
+            >
+              <Plus size={18} /> {addElementButtonLabel}
+            </Button>
+          </DialogTrigger>
+        ))}
     </div>
   );
 }
