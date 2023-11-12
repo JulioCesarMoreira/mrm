1a0f77bd8848ab277362c51ddddec41e88eb919b
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2023-11-12 19:45:05 -0300

    feat(Frontend/Services): added services form helper

--- frontend/src/pages/Services/ServicesFormPage.tsx
+++ frontend/src/pages/Services/ServicesFormPage.tsx
@@ -165,8 +165,29 @@ function ServicesFormPage(): ReactElement {
                 </DialogTrigger>
 
                 <DialogContent className="min-w-fit">
-                  <DialogHeader>Ajuda: Cadastro de serviço</DialogHeader>
-                  algm me ajuda
+                  <DialogHeader>Ajuda: Cadastro/Edição de serviço</DialogHeader>
+                  <div className="text-gray-scale-300 flex w-[580px] min-w-[580px] flex-col gap-4 pl-6">
+                    <ul className="list-disc space-y-4">
+                      <li>
+                        Preencha sua proposta de serviço com as informações do
+                        poço e os itens de serviço que serão utilizados para
+                        realiza-lá. Para escolher itens, basta primeiro
+                        selecionar a respectiva categoria desejada.
+                      </li>
+                      <li>
+                        Há também o espaço para anexos na parte inferior
+                        esquerda, onde será possível visualizar, remover ou
+                        adicionar conforme a necessidade.
+                      </li>
+
+                      <li className="mt-4 font-semibold">
+                        <hr className="bg-gray-scale-600 mb-4 w-full" />
+                        Atenção: para salvar um serviço, é necessário preencher
+                        os campos sobre o Poço, assim como selecionar um Cliente
+                        e informar Desconto e a Garantia.
+                      </li>
+                    </ul>
+                  </div>
                 </DialogContent>
               </Dialog>
             </div>
