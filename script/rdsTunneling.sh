9c15d9241f230f8c58d5ec6110c340c78cf9dffa
JulioCesarMoreira <55124442+JulioCesarMoreira@users.noreply.github.com>
2024-04-23 10:49:41 -0300

    Update rdsTunneling.sh

--- script/rdsTunneling.sh
+++ script/rdsTunneling.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-SSH_ENDPOINT='34.216.175.99'
-SSH_USER='ec2-user'
-DOCKER_IP='172.17.0.1'
+SSH_ENDPOINT=
+SSH_USER=
+DOCKER_IP=
 
 chmod 600 ~/.ssh/mrm-rds-tunnel.pem
 
-ssh -4 -N -L $DOCKER_IP:3306:mrm-mysql-instance-1.ccprilvyxwrp.us-west-2.rds.amazonaws.com:3306 $SSH_USER@$SSH_ENDPOINT -i ~/.ssh/mrm-rds-tunnel.pem
+ssh -4 -N -L $DOCKER_IP:3306:mrm-mysql-instance-1.${SECRET}.us-west-2.rds.amazonaws.com:3306 $SSH_USER@$SSH_ENDPOINT -i ~/.ssh/mrm-rds-tunnel.pem
